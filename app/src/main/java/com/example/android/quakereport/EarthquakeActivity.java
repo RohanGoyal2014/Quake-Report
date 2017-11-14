84a288c7f22fb6d5a24c5004ca6dbdc804865e3e
Ardulous <rohan.me2014@gmail.com>
2017-11-14 21:29:35 +0530

    Added Async Tasks-Multithreading

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,34 +15,76 @@
  */
 package com.example.android.quakereport;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-
+    public static String URLString = new String("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10");
+    ListView earthquakeListView;
+    EarthquakeAdapter Eadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        ArrayList<entity> earthquakes = new ArrayList<>();
-        earthquakes=queryutils.extractEarthquakes();
+        //ArrayList<entity> earthquakes = new ArrayList<>();
+        //earthquakes=queryutils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
-        ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeListView = (ListView) findViewById(R.id.list);
 
 
-        EarthquakeAdapter Eadapter=new EarthquakeAdapter(this,earthquakes);
-
+        //Eadapter=new EarthquakeAdapter(this,earthquakes);
+        new NetworkConnection().execute(URLString);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
+        //earthquakeListView.setAdapter(Eadapter);
+    }
+    private void updateUI()
+    {
+        if(earthquakeListView==null || Eadapter==null)
+        {
+            Log.e("UIUpdateProblem","Problem in updating UI from thread");
+        }
         earthquakeListView.setAdapter(Eadapter);
+    }
+    private class NetworkConnection extends AsyncTask<String,Void,ArrayList<entity>>
+    {
+        @Override
+        protected ArrayList<entity> doInBackground(String... strings) {
+            if(strings.length<1 || strings[0]==null)
+            {
+                return null;
+            }
+            //URL url=queryutils.getURLfromString(strings[0]);
+            String JSONResponse=queryutils.getJSONResponse(strings[0]);
+            ArrayList<entity> arrayList=new ArrayList<>();
+            if(JSONResponse!=null) {
+                arrayList = queryutils.extractEarthquakes(JSONResponse);
+            }
+            return arrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<entity> equakes) {
+            super.onPostExecute(equakes);
+            if(equakes==null)
+            {
+                return;
+            }
+            Eadapter=new EarthquakeAdapter(EarthquakeActivity.this,equakes);
+            updateUI();
+        }
     }
 }
